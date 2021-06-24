f991e8eab49874a4335533eed85d63033508010f
Pix3lAssassin <th3koolperson@gmail.com>
2021-06-24 17:14:39 -0600

    finished file prep for etl

--- helper/loadData.js
+++ helper/loadData.js
@@ -3,6 +3,7 @@ const readline = require('readline');
 const path = require('path');
 
 const dataDir = path.join(__dirname, '../data');
+const { Review } = require('../server/models/reviewModel');
 
 const etlLoad = (filename, model, callbacks) => {
   const rl = readline.createInterface({
@@ -11,46 +12,46 @@ const etlLoad = (filename, model, callbacks) => {
     terminal: false
   });
 
-  // const filenameArr = filename.split('.');
-  // const newFileName = `${filenameArr[0]}_cleaned.${filenameArr[1]}`;
+  const filenameArr = filename.split('.');
+  const newFileName = `${filenameArr[0]}_cleaned.${filenameArr[1]}`;
+  const writeStream = fs.createWriteStream(path.join(dataDir, newFileName), { encoding: 'utf8', flags: 'w' });
 
-  // const writeStream = fs.createWriteStream(path.join(dataDir, newFileName), { encoding: 'utf8', flags: 'w' });
   // eslint-disable-next-line no-plusplus, no-param-reassign
   const lineCounter = ((i = 0) => () => ++i)();
-  let dateIndex;
   let header;
+
   rl.on('line', (line, lineno = lineCounter()) => {
     if (lineno > 1) {
       const values = line.split(',');
 
-      const utcSeconds = Number(values[dateIndex]) / 1000;
-      const d = new Date(0);
-      d.setUTCSeconds(utcSeconds);
+      const row = [];
 
-      values[dateIndex] = d.toISOString();
-      const newline = values.join(',');
+      header.forEach((value, index) => {
+        row[index] = callbacks[index](values[index]);
+      });
+
       if (lineno % 10000 === 0) {
         // eslint-disable-next-line no-console
         console.log(`Completed: ${lineno}`);
       }
 
-      writeStream.write(`${newline}\n`);
+      // model.create(row)
+      //   .then((theModel) => {
+      //     console.log(theModel);
+      //   })
+      //   .catch((err) => {
+      //     console.log(err);
+      //   });
+      writeStream.write(`${row.join(',')}\n`);
     } else {
       header = line.split(',');
-
-      for (let i = 0; i < header.length; i += 1) {
-        if (header[i] === 'date') {
-          dateIndex = i;
-          break;
-        }
-      }
       writeStream.write(`${line}\n`);
     }
   });
 
   rl.on('close', () => {
     // eslint-disable-next-line no-console
-    console.log('Completed Conversion');
+    console.log('Completed Transfer');
     writeStream.close();
   });
 };
@@ -67,12 +68,15 @@ const cleanDate = (value) => {
 
 const limitLength = (limit) => (value) => {
   if (value.length > limit) {
-    value.slice(0, limit);
+    const newValue = `"${value.slice(1, limit - 1)}"`;
+    return newValue;
   }
+  return value;
 };
 
 etlLoad(
   'reviews.csv',
+  Review,
   [
     identity,
     identity,
--- server/models/reviewModel.js
+++ server/models/reviewModel.js
@@ -3,22 +3,93 @@ const { Sequelize, DataTypes } = require('sequelize');
 const db = new Sequelize('reviews_db', 'reviews_user', 'user_review', {
   host: 'localhost',
   dialect: 'postgres',
+  logging: false,
   define: {
     timestamps: false,
     freezeTableName: true
   }
 });
 
-exports.Review = db.define('item', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  quantity: {
+exports.Review = db.define('review', {
+  product_id: {
     type: DataTypes.INTEGER,
     allowNull: false
   },
-  completed: {
-    type: DataTypes.BOOLEAN
+  rating: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  summary: {
+    type: DataTypes.STRING(60),
+    allowNull: false
+  },
+  body: {
+    type: DataTypes.STRING(1000),
+    allowNull: false
+  },
+  recommend: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  },
+  reported: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  },
+  reviewer_name: {
+    type: DataTypes.STRING(60),
+    allowNull: false
+  },
+  reviewer_email: {
+    type: DataTypes.STRING(60),
+    allowNull: false
+  },
+  response: {
+    type: DataTypes.STRING(1000),
+    allowNull: true
+  },
+  helpfulness: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+});
+
+exports.Photo = db.define('photo', {
+  url: {
+    type: DataTypes.STRING(2000),
+    allowNull: false
+  },
+  review_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+});
+
+exports.Characteristic = db.define('characteristic', {
+  product_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING(7),
+    allowNull: false
+  }
+});
+
+exports.ReviewCharacteristic = db.define('review_characteristic', {
+  review_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  value: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  characteristic_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
   }
 });
