4d156141021c60eb23c039b725a6d80c38cc2443
Justin <th3koolperson@gmail.com>
2021-06-25 15:45:14 -0600

    added post route(incomplete) and updated data load sql scripts

--- schema/reviewLoad.sql
+++ schema/reviewLoad.sql
@@ -1,3 +1,15 @@
-COPY review
-FROM '/home/pix3llord/projects/altitude-apparel-reviews-api/data/reviews_cleaned.csv'
+COPY review (id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness)
+FROM '/home/pixellord/projects/altitude-apparel-reviews-api/data/reviews_cleaned.csv'
+WITH(FORMAT CSV, DELIMITER ',', HEADER TRUE);
+
+COPY photo (id, review_id, url)
+FROM '/home/pixellord/projects/altitude-apparel-reviews-api/data/reviews_photos.csv'
+WITH(FORMAT CSV, DELIMITER ',', HEADER TRUE);
+
+COPY characteristic (id, product_id, name)
+FROM '/home/pixellord/projects/altitude-apparel-reviews-api/data/characteristics.csv'
+WITH(FORMAT CSV, DELIMITER ',', HEADER TRUE);
+
+COPY review_characteristic (id, characteristic_id, review_id, value)
+FROM '/home/pixellord/projects/altitude-apparel-reviews-api/data/characteristic_reviews.csv'
 WITH(FORMAT CSV, DELIMITER ',', HEADER TRUE);
--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -8,13 +8,13 @@ const {
 
 const router = express.Router();
 
-// eslint-disable-next-line no-unused-vars
 router.get('/', (req, res) => {
   const page = req.query.page || 1;
   const count = req.query.count || 5;
 
   if (page < 1 || count < 1 || !req.query.product_id) {
     res.sendStatus(422);
+    return;
   }
 
   const sorts = {
@@ -35,7 +35,60 @@ router.get('/', (req, res) => {
     .then((results) => {
       res.status(200).send(results);
     })
-    .catch((err) => res.send(err));
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+const validateCharacteristicsObj = (charObj) => {
+  const keys = Object.keys(charObj);
+  for (let i = 0; i < keys.length; i += 1) {
+    const num = Number(keys[i]);
+    if (!Number.isInteger(num) || num < 1) {
+      return false;
+    }
+  }
+  return true;
+};
+
+router.post('/', (req, res) => {
+  const {
+    product_id: productId,
+    rating,
+    summary,
+    body,
+    recommend,
+    name,
+    email,
+    photos,
+    characteristics
+  } = req.body;
+
+  if (!productId || !rating
+      || rating < 1 || rating > 5
+      || !summary || !body || !recommend
+      || !name || !email || !photos
+      || Array.isArray(photos) || !characteristics
+      || validateCharacteristicsObj(characteristics)) {
+    res.sendStatus(422);
+    return;
+  }
+
+  Review.findAll({
+    where: {
+      product_id: req.query.product_id,
+      reported: false
+    },
+    limit: count,
+    offset: count * (page - 1),
+    order: sorts[req.query.sort || 'relevant']
+  })
+    .then((results) => {
+      res.status(200).send(results);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
 module.exports = router;
